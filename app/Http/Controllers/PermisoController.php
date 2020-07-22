03979b2ee03fcd29c2e2ca774e1c58985f1ab365
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2020-07-21 19:45:01 -0600

    .
    .

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -13,7 +13,7 @@ class PermisoController extends Controller
      */
     public function index()
     {
-        //
+        return view('permiso');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});     
+});
+
+Route::get('permiso', 'PermisoController@index');//el @ es para llamar metodos de un controlador.
+
